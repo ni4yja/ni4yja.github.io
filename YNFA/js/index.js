e90f5a7428b2f77e16eb862db8bb4808c04f0eaf
Liuba Kuibida <ni4yja@gmail.com>
2018-08-11 13:50:47 +0300

    add layout

---
+++ YNFA/js/index.js
@@ -0,0 +1,42 @@
+$(".header-menu").on("click","a", function (event) {
+	event.preventDefault();
+	var id  = $(this).attr('href'),
+	top = $(id).offset().top;
+	$('body,html').animate({scrollTop: top}, 1000);
+});
+
+$('#toggleMenu').click(function() {
+	$('.header-menu').toggleClass('responsive');
+});
+
+var features = new Swiper('.swiper-container', {
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+  	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true
+ 	}
+});
+
+var names = ['Interview: Loris Karius', 'Interview: Sadio Mané', 'Interview: Mohamed Salah'];
+
+var players = new Swiper('.player-item', {
+	slidesPerView: 1,
+	pagination: {
+			el: '.player-list',
+			clickable: true,
+			renderBullet: function (index, className) {
+				return '<div class="' + className + '">' + (names[index]) + 
+				'</div>';
+			},
+		}
+});
+
+//Click Logo To Scroll To Top
+$('.lfc-footer .logo').on('click', () => {
+	$('html,body').animate({
+		scrollTop: 0
+	},700);
+});
